f17b82a8fd4f4eb0e4b88f3fde12b6540b6fec47
Daniel <daniel.chavez@kcl.ac.uk>
2023-12-03 19:09:41 +0000

    first committ

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     },
     baseUrl: "garden.movingpixel.net",
     ignorePatterns: ["**/private", "templates", ".obsidian"],
-    defaultDateType: "published",
+    defaultDateType: "created",
     theme: {
       typography: {
         header: "Schibsted Grotesk",
